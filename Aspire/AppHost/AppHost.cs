b81fd461a4387ef19c0f6ac095b326420793f337
qalshakhoori <qalshakhoori@moic.gov.bh>
2025-08-25 11:30:41 +0300

    Adding catalog service to Aspire

--- Aspire/AppHost/AppHost.cs
+++ Aspire/AppHost/AppHost.cs
@@ -2,4 +2,6 @@ var builder = DistributedApplication.CreateBuilder(args);
 
 // Add projects and cloud-related services to the container.
 
+builder.AddProject<Projects.Catalog>("catalog");
+
 builder.Build().Run();
